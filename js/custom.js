a68213f4a01b3bfdf7a688f6cbdd16d30c43db75
Kristi Madhi <kristimadhi@gmail.com>
2016-06-21 12:39:41 +0200

    Update

--- js/custom.js
+++ js/custom.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 	
     $(window).scroll(function () {
         if ($(window).scrollTop() > 400) {
-            $("#navigation").css("background-color","#0EB493");
+            $("#navigation").css("background-color","#13a7b4");
         } else {
             $("#navigation").css("background-color","rgba(16, 22, 54, 0.2)");
         }

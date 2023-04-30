e866d434b02f16ec59fb9917cbe723e77a9bfef0
Kristi Madhi <madkrist@publicisgroupe.net>
2023-04-30 20:49:54 +0200

    Changes

--- form/Price-Form-Send.php
+++ form/Price-Form-Send.php
@@ -15,11 +15,11 @@ if(isset($_POST)) {
 	$mail->CharSet = "utf-8";
 	$mail->Host = "mail.example.com"; // web server host information
 	$mail->SMTPAuth = true;    // It must be false if not to login with SMTP_username.
-	$mail->Username = "example@example.com"; // e-mail username
+	$mail->Username = "info@gtr4cloud.de"; // e-mail username
 	$mail->Password = "LgI(mj0z6]ym"; // Mail password
-	$mail->From = "example@example.com"; // Who is the mail from?
+	$mail->From = "info@gtr4cloud.de"; // Who is the mail from?
 	$mail->FromName = "Web Form Mail | https://templatemonster.com/author/garantiwebt"; // Mailbox Incoming Header
-	$mail->AddAddress("example@example.com"); // address to send mail
+	$mail->AddAddress("info@gtr4cloud.de"); // address to send mail
 	$mail->Subject = "Templatemonster Garantiwebt Contact Form";
 	$mail->IsHTML(true);
 	$mail->Body = "Mail İçeriği ; <br>  İsim Soyisim : " .$_isim." <br> E-Posta : " .$_eposta." <br> Telefon : ".$_telefon." <br>";
